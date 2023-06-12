ea38809f4d36a64b9e72e109802e2b9e60740faf
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-12 22:57:22 +1000

    SignUp form completed

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -46,6 +46,131 @@ const SignUpForm = ({ navigation }) => {
         }
     }
     return (
-        <View style={sty}
+        <View style={styles.wrapper}>
+            <Formik
+                initialValues={{ email: '', password: '', username: '' }}
+                onSubmit={(values) => onSignup(values.email, values.password, values.username)}
+                validationSchema={signUpFormSchema}
+                validateOnMount={true}
+            >
+                {({
+                    handleBlur,
+                    handleChange,
+                    handleSubmit,
+                    values,
+                    errors,
+                    isValid,
+                }) => (
+                    <>
+                        <View style={[ styles.inputField, {
+                            borderColor: values.email.length < 1 || Validator.validate(values.email)
+                                ? '#ccc'
+                                : 'red',
+                        },
+                        ]}>
+                            <TextInput
+                                placeholderTextColor="#444"
+                                placeholder="Phone Number, username, or email"
+                                autoCapitalize="none"
+                                keyboardType="email-address"
+                                textContentType="emailAddress"
+                                autoFocus={true}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                            />
+                        </View>
+                        <View style={[ styles.inputField, {
+                            borderColor: 1 > values.username.length || values.username.length >= 3
+                                ? '#ccc'
+                                : 'red',
+                        },
+                        ]}>
+                            <TextInput
+                                placeholderTextColor="#444"
+                                placeholder="Username"
+                                autoCapitalize="none"
+                                textContentType="username"
+                                autoFocus={true}
+                                onChangeText={handleChange('username')}
+                                onBlur={handleBlur('username')}
+                                value={values.username}
+                            />
+                        </View>
+
+                        <View style={[ styles.inputField, {
+                            borderColor: 1 > values.password.length || values.password.length >= 6
+                                ? '#ccc'
+                                : 'red',
+                        },
+                        ]}>
+                            <TextInput
+                                placeholderTextColor="#444"
+                                placeholder="Password"
+                                autoCapitalize="none"
+                                textContentType="password"
+                                autoFocus={false}
+                                secureTextEntry={true}
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                            />
+                        </View>
+
+                        <View style={{ alignItems: 'flex-end', marginBottom: 10 }}>
+                            <Text style={{ color: '#6bb8f5' }}>Forget Password</Text>
+                        </View>
+                        <Pressable
+                            titleSize={20}
+                            style={styles.button}
+                            onPress={handleSubmit}
+                            disabled={!isValid}
+                        >
+                            <Text style={styles.buttonText}>Sign Up</Text>
+                        </Pressable>
+
+                        <View style={styles.signupContainer}>
+                            <Text>Already a member? </Text>
+                            <TouchableOpacity onPress={() => navigation.goBack()}>
+                                <Text style={{ color: '#6bb0f5' }}>Login Up</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                )}
+            </Formik>
+        </View>
     )
 }
+
+export default SignUpForm
+
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginTop: 80,
+    },
+    inputField: {
+        borderRadius: 4,
+        padding: 12,
+        backgroundColor: '#FAFAFA',
+        marginBottom: 10,
+        borderWidth: 1,
+    },
+    button: {
+        backgroundColor: '#0096f6',
+        alignItems: 'center',
+        minHeight: 42,
+        borderRadius: 4,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 600,
+        fontSize: 20,
+    },
+    signupContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        marginTop: 50,
+        justifyContent: 'center',
+    },
+})
