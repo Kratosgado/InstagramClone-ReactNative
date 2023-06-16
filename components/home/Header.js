690102a038b298f1adb3682c6bdafa74e410d20c
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-17 08:48:24 +1000

    complete header component creation

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 const Header = () => {
     return (
-        <View>
-            <Image
-                style={styles.logo}
-                source={require('../../assets/header-logo.png')}
-            />
+        <View style={styles.headerContainer}>
+            <TouchableOpacity>
+                <Image
+                    style={styles.logo}
+                    source={require('../../assets/header-logo.png')}
+                />
+            </TouchableOpacity>
+            <View style={styles.iconContainer}>
+                <TouchableOpacity>
+                    <Image
+                        style={styles.icon}
+                        source={{
+                            uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png'
+                        }}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image
+                        style={styles.icon}
+                        source={{
+                            uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png'
+                        }}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>15</Text>
+                    </View>
+                    <Image
+                        style={styles.icon}
+                        source={{
+                            uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png'
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -15,9 +46,42 @@ const Header = () => {
 export default Header
 
 const styles = StyleSheet.create({
+    headerContainer: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        alignItems: 'center',
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
     logo: {
         width: 100,
         height: 50,
         resizeMode: 'contain'
-    }
+    },
+    icon: {
+        height: 30,
+        width: 30,
+        resizeMode: 'contain',
+        marginLeft: 8
+    },
+    unreadBadge: {
+        backgroundColor: '#ff3250',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100,
+    },
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: 600,
+    },
 })
