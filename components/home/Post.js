bcf09394089fd4084ee5e56cbec689e2a47f5c9a
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-18 05:03:44 +1000

    install react-native-elements for divider

---
+++ components/home/Post.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Divider } from 'react-native-elements'
+
+
+const Post = ({ post }) => {
+    return (
+        <View style={{ marginBottom: 30 }}>
+
+            <Text style={{ color: 'white' }}>Post</Text>
+        </View>
+    )
+}
+
+export default Post
