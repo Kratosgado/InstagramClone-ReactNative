39df40eb20722664dbec84a79ff7c2059a1351b1
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-21 02:24:12 +1000

    PostHeader component done

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { Divider } from 'react-native-elements'
 
@@ -6,10 +6,46 @@ import { Divider } from 'react-native-elements'
 const Post = ({ post }) => {
     return (
         <View style={{ marginBottom: 30 }}>
-
-            <Text style={{ color: 'white' }}>Post</Text>
+            <Divider width={1} orientation='vertical' />
+            <PostHeader post={post} />
+            <PostImage post={post} />
         </View>
     )
 }
 
+const PostHeader = ({ post }) => (
+    <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 5,
+        alignItems: 'center'
+    }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image source={{ uri: post.profile_picture }} style={styles.story} />
+            <Text style={{ color: 'white', marginLeft: 5, fontWeight: '700' }}>{post.user}</Text>
+        </View>
+        <Text style={{ color: 'white', fontWeight: '900' }}>
+            ...
+        </Text>
+    </View>
+)
+
+const PostImage = ({ post }) => (
+    <Image source={{ uri: post.imageUrl }} style={{
+        height: '100%',
+        resizeMode: 'cover'
+    }} />
+)
+
 export default Post
+
+const styles = StyleSheet.create({
+    story: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 1.6,
+        borderColor: '#ff8501'
+    }
+})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, ScrollView, Text, StyleSheet } from "react-native";
 import Header from '../components/home/Header'
 import Stories from "../components/home/Stories";
 import Post from "../components/home/Post";
+
+import { POSTS } from '../data/posts';
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header />
             <Stories />
-            <Post />
+            <ScrollView>
+                {POSTS.map((post, index) => (
+                    <Post post={post} key={index} />
+                ))}
+            </ScrollView>
         </SafeAreaView>
     )
 }
